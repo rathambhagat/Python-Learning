2c92f6e119e0e15cb2d45e6efac8a005da8c3ea0
rathambhagat <prathambhagat720@gmail.com>
2026-03-13 19:33:09 +0530

    Python learning(Day-2)

--- Phase-1/Functions and variables/Functions and variables.py
+++ Phase-1/Functions and variables/Functions and variables.py
@@ -33,3 +33,42 @@
 # b = int(input("Enter your second number: "))
 # print(f"The sum of {a} and {b} is: {a + b}") 
 
+#floats 
+# x = float(input("What's x? "))
+# y = float(input("What's y? "))
+# print(x+y) # print float value 
+# z = round(x+y)
+# print(z) # rounding floats
+# print(f"{z:,}") # giving it a standard comma 
+# z = round(x/y,2) # rounding to 2 points the dividing value
+# z = x/y
+# print(f"{z:.2f}") # f-string method to round integer
+
+#Defining your own function
+# def hello():
+#     print("Hello!")
+# name = input("What's Your name? ")
+# hello()
+# print(name)
+
+# passing a value so we can create a custom output
+# def hello(to):
+#     print("Hello!,", to)
+# name = input("What's your name? ")
+# hello(name)
+
+# we called function before 
+# def hello(to = "world"):
+#     print("Hello!,", to)
+# hello()
+# name = input("What's Your name? ")
+# hello(name)
+
+#calculator function
+# def main():
+#     x = int(input("What's is x? "))
+#     print("x squared is ",square(x))
+# def square(n):
+#     return n*n
+#     return pow(2,n) # alternative syntax
+# main()
