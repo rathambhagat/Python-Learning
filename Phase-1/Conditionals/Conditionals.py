ff8f3fdc9fc10ecffa26a5bc3bc69e5e80fa3320
rathambhagat <prathambhagat720@gmail.com>
2026-03-13 23:55:20 +0530

    Python Learning(Day-2)

--- Phase-1/Conditionals/Conditionals.py
+++ Phase-1/Conditionals/Conditionals.py
@@ -183,3 +183,134 @@ else:
 #             +-------------------+
 #             |      🟥 End       |
 #             +-------------------+
+
+# grade 
+score = int(input("Enter your score(0-100): "))
+if 90 <= score <= 100: #alternative syntax
+	print("Your Grade is A")
+elif 80 <= score < 90: #alternative syntax
+	print("Your Grade is B")
+elif 70 <= score < 80: #alternative syntax
+	print("Your Grade is C")
+elif 60 <= score < 70: #alternative syntax
+	print("Your Grade is D")
+else:
+	print("Your Grade is F")
+
+# Flowchart for grade code
+# Color Legend: 🔵 Start/Input | 🟨 Decision | 🟩 Output | 🟥 End
+#
+#            +----------------------+
+#            |      🔵 Start        |
+#            +----------------------+
+#                      |
+#                      v
+#            +----------------------+
+#            | 🔵 Input score 0-100 |
+#            +----------------------+
+#                      |
+#                      v
+#            +----------------------+
+#            | 🟨 90 <= score <=100? |
+#            +----------------------+
+#               | Yes          | No
+#               v              v
+#   +----------------------+   +----------------------+
+#   | 🟩 Print Grade A     |   | 🟨 80 <= score < 90? |
+#   +----------------------+   +----------------------+
+#               |                  | Yes          | No
+#               |                  v              v
+#               |      +----------------------+   +----------------------+
+#               |      | 🟩 Print Grade B     |   | 🟨 70 <= score < 80? |
+#               |      +----------------------+   +----------------------+
+#               |                  |                  | Yes          | No
+#               |                  |                  v              v
+#               |                  |      +----------------------+   +----------------------+
+#               |                  |      | 🟩 Print Grade C     |   | 🟨 60 <= score < 70? |
+#               |                  |      +----------------------+   +----------------------+
+#               |                  |                  | Yes          | No
+#               |                  |                  v              v
+#               |                  |      +----------------------+   +----------------------+
+#               |                  |      | 🟩 Print Grade D     |   | 🟩 Print Grade F     |
+#               |                  |      +----------------------+   +----------------------+
+#               |                  |                  |                  |
+#               +------------------+------------------+------------------+
+#                                                     |
+#                                                     v
+#                                            +----------------------+
+#                                            |       🟥 End         |
+#                                            +----------------------+
+
+# score = int(input("Enter your score(0-100): "))
+if score >= 90:
+	print("Your Grade is A")
+elif score >= 80:
+	print("Your Grade is B")
+elif score >= 70:
+	print("Your Grade is C")
+elif score >= 60:
+	print("Your Grade is D")
+else:
+	print("Your Grade is F")
+
+# Flowchart for grade code using >=
+# Color Legend: 🔵 Start/Input | 🟨 Decision | 🟩 Output | 🟥 End
+#
+#            +----------------------+
+#            |      🔵 Start        |
+#            +----------------------+
+#                      |
+#                      v
+#            +----------------------+
+#            | 🔵 Input score 0-100 |
+#            +----------------------+
+#                      |
+#                      v
+#            +----------------------+
+#            |    🟨 score >= 90?    |
+#            +----------------------+
+#               | Yes          | No
+#               v              v
+#   +----------------------+   +----------------------+
+#   | 🟩 Print Grade A     |   |    🟨 score >= 80?   |
+#   +----------------------+   +----------------------+
+#               |                  | Yes          | No
+#               |                  v              v
+#               |      +----------------------+   +----------------------+
+#               |      | 🟩 Print Grade B     |   |    🟨 score >= 70?   |
+#               |      +----------------------+   +----------------------+
+#               |                  |                  | Yes          | No
+#               |                  |                  v              v
+#               |                  |      +----------------------+   +----------------------+
+#               |                  |      | 🟩 Print Grade C     |   |    🟨 score >= 60?   |
+#               |                  |      +----------------------+   +----------------------+
+#               |                  |                  | Yes          | No
+#               |                  |                  v              v
+#               |                  |      +----------------------+   +----------------------+
+#               |                  |      | 🟩 Print Grade D     |   | 🟩 Print Grade F     |
+#               |                  |      +----------------------+   +----------------------+
+#               |                  |                  |                  |
+#               +------------------+------------------+------------------+
+#                                                     |
+#                                                     v
+#                                            +----------------------+
+#                                            |       🟥 End         |
+#                                            +----------------------+
+
+x = int(input("What's x? "))
+if x % 2 == 0:
+    print("Even")
+else:
+	print("Odd")
+
+def main():
+	x = int(input("What's x? "))
+	if x % 2 == 0:
+		print("Even")
+	else:
+		print("Odd")
+
+def is_even(n):
+	return n % 2 == 0
+
+main()
