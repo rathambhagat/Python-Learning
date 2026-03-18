3c597a9a44f38a8e4f4a203ac68b66592fdf7b28
rathambhagat <prathambhagat720@gmail.com>
2026-03-19 01:35:14 +0530

    Python Learning(Day-5)

--- Phase-2/Easy/Hackerrank(easy).py
+++ Phase-2/Easy/Hackerrank(easy).py
@@ -2,3 +2,27 @@
 
 # Problem 1: Print "Hello, World!"
 print("Hello, World!")
+
+# Problem 2: 
+"""
+Task
+Given an integer,n , perform the following conditional actions:
+
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+"""
+n = int(input())
+if n < 1 or n > 100:
+    print("Please enter a number between 1 and 100.")
+else:
+    if n % 2 == 1:
+        print("Weird")
+    elif n >= 2 and n <= 5:
+        print("Not Weird")
+    elif n >= 6 and n <= 20:
+        print("Weird")
+    elif n > 20:
+        print("Not Weird")
+
