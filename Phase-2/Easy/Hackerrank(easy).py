31e175c937df0d281fd4c5ae462186957a581ed5
rathambhagat <prathambhagat720@gmail.com>
2026-03-23 23:00:14 +0530

    Python Learning(Day-12)

--- Phase-2/Easy/Hackerrank(easy).py
+++ Phase-2/Easy/Hackerrank(easy).py
@@ -252,5 +252,45 @@ if __name__ == '__main__':
 
 # Problem Statement 13
 """
+Consider a list (list = []). You can perform the following commands:
+insert i e: Insert integer e at position i.
+print: Print the list.
+remove e: Delete the first occurrence of integer e.
+append e: Insert integer  at the end of the list.
+sort: Sort the list.
+pop: Pop the last element from the list.
+reverse: Reverse the list.
+Initialize your list and read in the value of n followed by n lines of commands where each command will be of the 7 types listed above. 
+Iterate through each command in order and perform the corresponding operation on your list.
+"""
+if __name__ == '__main__':
+    N = int(input())
+    lst = []
+    for _ in range(N):
+        cmd = input().split()
+        op = cmd[0]
+        if op == "insert":
+            i = int(cmd[1])
+            e = int(cmd[2])
+            lst.insert(i, e)
+        elif op == "print":
+            print(lst)
+        elif op == "remove":
+            e = int(cmd[1])
+            lst.remove(e)
+        elif op == "append":
+            e = int(cmd[1])
+            lst.append(e)
+        elif op == "sort":
+            lst.sort()
+        elif op == "pop":
+            lst.pop()
+        elif op == "reverse":
+            lst.reverse()       
+
+# Problem Statement 14
+"""
 
 """
+
+
