863baa68dffbfec53caa6b5112853ad6dc6309c9
rathambhagat <prathambhagat720@gmail.com>
2026-03-23 20:01:03 +0530

    Learning Python(Day-12)

--- Phase-2/Easy/Hackerrank(easy).py
+++ Phase-2/Easy/Hackerrank(easy).py
@@ -232,4 +232,24 @@ def student():
 if __name__ == '__main__':
     student()
 
+# Problem Statement 12
+"""
+The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. 
+Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+"""
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    marks = student_marks[query_name]          
+    average = sum(marks) / len(marks)       
+    print("{:.2f}".format(average))
 
+# Problem Statement 13
+"""
+
+"""
