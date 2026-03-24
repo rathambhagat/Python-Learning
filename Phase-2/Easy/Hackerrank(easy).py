5d202fefe55eb0c011982d3c8fae8891f1621e1c
rathambhagat <prathambhagat720@gmail.com>
2026-03-24 20:20:11 +0530

    Python Learning(Day-14)

--- Phase-2/Easy/Hackerrank(easy).py
+++ Phase-2/Easy/Hackerrank(easy).py
@@ -330,3 +330,23 @@ if __name__ == '__main__':
     first_name = input()
     last_name = input()
     print_full_name(first_name, last_name)
+
+# Problem Statement 17
+"""
+Read a given string, change the character at a given index and then print the modified string.
+"""
+def mutate_string(string, position, character):
+    lst = list(string)
+    lst[position] = character
+    return ''.join(lst)
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
+
+# Problem Statement 18
+"""
+
+"""
