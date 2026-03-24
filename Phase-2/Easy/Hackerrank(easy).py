08fcad8c315aed53fd496ae3f232c31a2a25ef7b
rathambhagat <prathambhagat720@gmail.com>
2026-03-24 23:08:17 +0530

    Python Learning(Day-14)

--- Phase-2/Easy/Hackerrank(easy).py
+++ Phase-2/Easy/Hackerrank(easy).py
@@ -367,3 +367,21 @@ if __name__ == '__main__':
     
     count = count_substring(string, sub_string)
     print(count)
+
+# Problem Statement 19
+"""
+You are given a string S.
+Your task is to find out if the string S contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
+In the first line, print True if S has any alphanumeric characters. Otherwise, print False.
+In the second line, print True if S has any alphabetical characters. Otherwise, print False.
+In the third line, print True if S has any digits. Otherwise, print False.
+In the fourth line, print True if S has any lowercase characters. Otherwise, print False.
+In the fifth line, print True if S has any uppercase characters. Otherwise, print False.
+"""
+if __name__ == '__main__':
+    s = input()
+    print(any(c.isalnum() for c in s))
+    print(any(c.isalpha() for c in s))
+    print(any(c.isdigit() for c in s))
+    print(any(c.islower() for c in s))
+    print(any(c.isupper() for c in s))
