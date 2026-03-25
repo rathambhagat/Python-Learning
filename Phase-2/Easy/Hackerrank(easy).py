4ca23d0cc0fe1e3aa61c56c36abac147bf78cce7
rathambhagat <prathambhagat720@gmail.com>
2026-03-25 23:53:17 +0530

    Python Learning(Day-15)

--- Phase-2/Easy/Hackerrank(easy).py
+++ Phase-2/Easy/Hackerrank(easy).py
@@ -458,4 +458,20 @@ french = set(map(int, input().split()))
 s = len(english.union(french))
 print(s)
 
-
+# Problem Statement 24
+"""
+The students of District College have subscriptions to English and French newspapers. 
+Some students have subscribed only to English, some have subscribed to only French and some have subscribed to both newspapers.
+You are given two sets of student roll numbers. One set has subscribed to the English newspaper, and the other set is subscribed to the French newspaper. 
+The same student could be in both sets. Your task is to find the total number of students who have subscribed to both the newspapers.
+Input Format
+The first line contains an integer,n , the number of students who have subscribed to the English newspaper.
+The second line contains n space separated roll numbers of those students.
+The third line contains b, the number of students who have subscribed to the French newspaper.
+The fourth line contains b space separated roll numbers of those students.
+"""
+n = int(input())
+english = set(map(int,input().split()))
+b = int(input())
+french = set(map(int,input().split()))
+print(len(english.intersection(french)))
