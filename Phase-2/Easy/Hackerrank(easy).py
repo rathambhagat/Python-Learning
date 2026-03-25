95a397ace6c795dfdd2ebb1b9b172090ad560a92
rathambhagat <prathambhagat720@gmail.com>
2026-03-25 23:52:14 +0530

    Python Learning(Day-15)

--- Phase-2/Easy/Hackerrank(easy).py
+++ Phase-2/Easy/Hackerrank(easy).py
@@ -399,3 +399,19 @@ if __name__ == '__main__':
     arr = list(map(int, input().split()))
     result = average(arr)
     print(result)
+    # Problem Statement 21
+"""
+Rupal has a huge collection of country stamps. She decided to count the total number of distinct country stamps in her collection. 
+She asked for your help. You pick the stamps one by one from a stack of N country stamps.
+Input Format:
+The first line contains an integer N, the total number of country stamps.
+The next N lines contains the name of the country where the stamp is from.
+Output the total number of distinct country stamps on a single line.
+"""
+n = int(input())
+Stamps = set()
+for i in range(n):
+    Stamps.add(input())
+print(len(Stamps))
+    
+
