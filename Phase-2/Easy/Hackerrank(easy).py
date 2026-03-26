8c84fe90d372a4b12950aace3ccb8b755bbc3fa6
rathambhagat <prathambhagat720@gmail.com>
2026-03-26 19:59:22 +0530

    Python Learning(Day-16)

--- Phase-2/Easy/Hackerrank(easy).py
+++ Phase-2/Easy/Hackerrank(easy).py
@@ -623,3 +623,16 @@ res = divmod(a,b)
 print(res[0])
 print(res[1])
 print(res)
+
+# Problem Statement 30
+"""
+You are given three integers: a, b, and m. Print two lines.
+On the first line, print the result of pow(a,b). On the second line, print the result of pow(a,b,m).
+Input Format
+The first line contains a, the second line contains b, and the third line contains m.
+"""
+a = int(input())
+b = int(input())
+m = int(input())
+print(pow(a,b))
+print(pow(a,b,m))
