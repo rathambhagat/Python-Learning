cf40adf35a1af4f41e8a7bdeae9393e238f6b51f
rathambhagat <prathambhagat720@gmail.com>
2026-03-29 21:36:00 +0530

    Python Learning(Day-20)

--- Phase-2/Easy/Hackerrank(easy).py
+++ Phase-2/Easy/Hackerrank(easy).py
@@ -948,3 +948,31 @@ print("WELCOME".center(M, '-'))
 for i in range(N-2, -1, -2):
     pattern = (".|." * i)
     print(pattern.center(M, '-'))
+
+# Problem Statement 40
+"""
+You are given a string and width.
+Your task is to wrap the string S into a paragraph of width w.
+Function Description
+Complete the wrap function in the editor below.
+wrap has the following parameters:
+string string: a long string
+int max_width: the width to wrap to
+Returns
+string: a single string with newline characters ('\n') where the breaks should be
+Input Format
+The first line contains a string, String.
+The second line contains the width, Width.
+Sample Input 0
+ABCDEFGHIJKLIMNOQRSTUVWXYZ4
+Sample Output 0
+ABCDEFGHIJKLIMNOQRSTUVWXYZ
+"""
+import textwrap
+
+def wrap(string, max_width):
+    return textwrap.fill(string, max_width)
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
