ae1c4d373ddc8e3b33c0ff8c98288c7ec1432bd5
rathambhagat <prathambhagat720@gmail.com>
2026-03-31 22:40:17 +0530

    making unsated changes

--- Phase-2/Easy/Hackerrank(easy).py
+++ Phase-2/Easy/Hackerrank(easy).py
@@ -977,30 +977,3 @@ if __name__ == '__main__':
     result = wrap(string, max_width)
     print(result)
 
-# Problem Statement 41
-"""
-Task
-Dr. John Wesley has a spreadsheet containing a list of student's IDs, marks, class and name.
-Your task is to help Dr. Wesley calculate the average marks of the students.
-Average Sum of all marks Total Students
-Note:
-1. Columns can be in any order. IDs, marks, class and name can be written in any order in the spreadsheet.
-2. Column names are ID, MARKS, CLASS and NAME. (The spelling and case type of these names won't change.)
-Input Format
-The first line contains an integer N, the total number of students.
-The second line contains the names of the columns in any order.
-The next N lines contains the marks, IDs, name and class, under their respective column names.
-Constraints
-0 < N < 100
-Output Format
-Print the average marks of the list corrected to 2 decimal places.
-"""
-n = int(input())
-columns = input().split()
-marks_index = columns.index('MARKS')
-total_marks = 0
-for _ in range(n):
-    data = input().split()
-    total_marks += int(data[marks_index])
-average = total_marks / n
-print(f"{average:.2f}")
